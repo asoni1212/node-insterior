d0ae1893c1ffd2e6e2440e09eb9552e5a8c5ab31
Atharva soni <sidsoni1212@gmail.com>
2023-08-09 10:33:25 +0530

    change database name

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req,res)=>{
 })
 
 app.get("/user", (req,res)=>{
-    const q = "SELECT * FROM USERS"
+    const q = "SELECT * FROM users"
     db.query(q,(err,data)=>{
        if (err) return res.json(err);
         return res.json(data)
